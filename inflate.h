c714e9cdc5a1481df57f72380cc02b3c865a19c5
wareya <wareya@gmail.com>
2023-12-21 23:17:00 -0500

    initial commit (with stuff i'll need to delete later)

---
+++ inflate.h
@@ -0,0 +1,274 @@
+
+#include <stdint.h> // basic types
+#include <stdlib.h> // size_t
+
+#include "buffers.h"
+
+
+void print_binary(uint64_t val, uint8_t size)
+{
+    for (size_t i = 0; i < size; i++)
+        printf("%c", val & (1 << (size - i - 1)) ? '1' : '0');
+}
+
+void build_code(uint8_t * code_lens, uint16_t * code_lits, uint16_t * code_by_len, size_t total_count)
+{
+    uint16_t min = 0;
+    uint16_t len_count[15] = {0};
+    for (size_t val = 0; val < total_count; val += 1)
+    {
+        uint8_t len = code_lens[val];
+        //printf("code len for %d: %d\n", val, len);
+        if (len)
+            len_count[len]++;
+    }
+    
+    for (size_t i = 0; i < 15; i += 1)
+    {
+        min += min + (len_count[i] << 1); // not a typo
+        code_by_len[i + 1] = min;
+    }
+    for (uint16_t val = 0; val < total_count; val += 1)
+    {
+        uint8_t len = code_lens[val];
+        if (len)
+        {
+            uint16_t code = code_by_len[len]++;
+            //printf("mapping ");
+            //print_binary(code, len);
+            //printf(" to %d\n", val);
+            code_lits[code] = val;
+        }
+    }
+}
+
+#define ASSERT_OR_BROKEN_FILE(expr,ret) { if (!(expr)) { *error = -1; printf("assert failed on line %d\n", __LINE__); return ret; } }
+#define ASSERT_OR_BROKEN_DECODER(expr,ret) { if (!(expr)) { *error = 1; printf("assert failed on line %d\n", __LINE__); return ret; } }
+
+uint16_t read_huff_code(bit_buffer * input, uint16_t * code_by_len, int * error)
+{
+    uint8_t code_len = 1;
+    uint16_t code = bit_pop(input);
+    while (code_len < 16 && code >= code_by_len[code_len])
+    {
+        code = (code << 1) | bit_pop(input);
+        code_len += 1;
+    }
+    ASSERT_OR_BROKEN_FILE(code != 0 || code_len < 16, code)
+    //printf("%d with code %d\n", code_lits[code], code);
+    return code;
+}
+
+void do_lz77(bit_buffer * input, byte_buffer * ret, uint16_t * code_lits, uint16_t * code_by_len, uint16_t * dist_code_lits, uint16_t * dist_code_by_len, int * error)
+{
+    int huff_error = 0;
+    uint16_t literal = 256;
+    //puts("reading literals...");
+    do
+    {
+        literal = code_lits[read_huff_code(input, code_by_len, &huff_error)];
+        ASSERT_OR_BROKEN_FILE(huff_error == 0,)
+        ASSERT_OR_BROKEN_FILE(literal <= 285,)
+        
+        if (literal < 256)
+        {
+            //printf("pushing a literal, %d\n", literal);
+            byte_push(ret, literal);
+        }
+        else if (literal > 256)
+        {
+            //puts("reading extra bits...");
+            uint8_t len_extra_bits = 0;
+            if (literal >= 261 && literal < 285)
+                len_extra_bits = (literal - 261) / 4;
+            uint16_t len_mins[29] = {3,4,5,6,7,8,9,10,11,13,15,17,19,23,27,31,35,43,51,59,67,83,99,115,131,163,195,227,258};
+            uint16_t len = len_mins[literal-257] + bits_pop(input, len_extra_bits);
+            
+            //printf("%d %d %d\n", literal, len, len_extra_bits);
+            
+            uint16_t dist_literal = dist_code_lits[read_huff_code(input, dist_code_by_len, &huff_error)];
+            ASSERT_OR_BROKEN_FILE(huff_error == 0,)
+            ASSERT_OR_BROKEN_FILE(dist_literal <= 29,)
+            
+            //printf("%d %d\n", dist_code, dist_literal);
+            
+            uint8_t dist_extra_bits = 0;
+            if (dist_literal >= 2)
+                dist_extra_bits = (dist_literal - 2) / 2;
+            uint16_t dist_mins[30] = {1,2,3,4,5,7,9,13,17,25,33,49,65,97,129,193,257,385,513,769,1025,1537,2049,3073,4097,6145,8193,12289,16385,24577};
+            uint16_t dist = dist_mins[dist_literal] + bits_pop(input, dist_extra_bits);
+            
+            //printf("lookback command: %d %d\n", len, dist);
+            //printf("(output len so far: %lld)\n", ret.len);
+            ASSERT_OR_BROKEN_FILE(dist <= ret->len,)
+            
+            for (size_t j = 0; j < len; j++)
+                byte_push(ret, ret->data[ret->len - dist]);
+        }
+        else
+            break;
+    } while (literal != 256);
+}
+// on error, error is set to nonzero. otherwise error is unset
+// positive error: bug in decoder
+// negative error: broken DEFLATE data
+byte_buffer do_inflate(byte_buffer * input_bytes, int * error)
+{
+    byte_buffer ret = {0, 0, 0, 0};
+    bit_buffer input = {*input_bytes, 0, 0};
+    
+    uint16_t static_lits[1 << 9];
+    memset(static_lits, 0xFF, sizeof(uint16_t) * (1 << 9));
+    for (uint16_t i = 0; i < 144; i++)
+        static_lits[i] = i + 48;
+    for (uint16_t i = 144; i < 256; i++)
+        static_lits[i] = i - 144 + 400;
+    for (uint16_t i = 256; i < 280; i++)
+        static_lits[i] = i - 256;
+    for (uint16_t i = 280; i < 288; i++)
+        static_lits[i] = i - 280 + 192;
+    
+    uint16_t static_by_len[16];
+    memset(static_by_len, 0, sizeof(uint16_t) * 16);
+    static_by_len[7] = 0x18;
+    static_by_len[8] = 0xC8;
+    static_by_len[9] = 0xFFFF;
+    
+    uint16_t static_dists[1 << 5];
+    memset(static_dists, 0xFF, (1 << 5) - 1);
+    for (uint16_t i = 0; i < 32; i++)
+        static_dists[i] = i;
+    
+    uint16_t static_dists_by_len[16];
+    memset(static_dists_by_len, 0, sizeof(uint16_t) * 16);
+    static_by_len[5] = 0xFFFF;
+    
+    while(1)
+    {
+        uint8_t final = bit_pop(&input);
+        uint8_t type = bits_pop(&input, 2);
+        if (type == 0)
+        {
+            bits_align_to_byte(&input);
+            uint16_t len = bits_pop(&input, 16);
+            uint16_t nlen = bits_pop(&input, 16);
+            ASSERT_OR_BROKEN_FILE(len == (uint16_t)~nlen, ret)
+            ASSERT_OR_BROKEN_FILE(input.byte_index + len <= input.buffer.len, ret)
+            bytes_push(&ret, &input.buffer.data[input.byte_index], len);
+            input.byte_index += len;
+            break;
+        }
+        else if (type == 1)
+        {
+            int lz77_error = 0;
+            do_lz77(&input, &ret, static_lits, static_by_len, static_dists, static_dists_by_len, &lz77_error);
+            ASSERT_OR_BROKEN_FILE(lz77_error == 0, ret)
+        }
+        else if (type == 2)
+        {
+            uint16_t len_count = bits_pop(&input, 5) + 257;
+            uint8_t dist_count = bits_pop(&input, 5) + 1;
+            uint8_t codelen_count = bits_pop(&input, 4) + 4;
+            
+            uint8_t inst_code_vals[19] = {16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15};
+            uint8_t inst_code_lens[19] = {0};
+            uint16_t inst_code_lits[1 << 7] = {0};
+            uint16_t inst_code_by_len[16] = {0};
+            
+            uint8_t max_len = 0;
+            for (uint16_t i = 0; i < codelen_count; i += 1)
+            {
+                uint8_t len = bits_pop(&input, 3);
+                //printf("loaded code len %d for value %d\n", len, inst_code_vals[i]);
+                inst_code_lens[inst_code_vals[i]] = len;
+            }
+            
+            build_code(inst_code_lens, inst_code_lits, inst_code_by_len, 19);
+            
+            for (size_t i = 0; i < 16; i += 1)
+            {
+                //print_binary(inst_code_by_len[i], i);
+                //puts("");
+                //printf("by len %d: %d\n", i, inst_code_by_len[i]);
+            }
+            
+            int huff_error = 0;
+            // parse compressed code lengths
+            uint8_t raw_code_lens[288 + 32] = {0};
+            for (size_t i = 0; i < len_count + dist_count; i += 1)
+            {
+                uint16_t inst = inst_code_lits[read_huff_code(&input, inst_code_by_len, &huff_error)];
+                ASSERT_OR_BROKEN_FILE(huff_error == 0, ret)
+                //printf("inst %d with code ", inst);
+                //print_binary(inst_code, inst_code_len);
+                //puts("");
+                
+                if (inst < 16)
+                    raw_code_lens[i] = inst;
+                else if (inst == 16)
+                {
+                    ASSERT_OR_BROKEN_FILE(i > 0, ret)
+                    uint8_t count = bits_pop(&input, 2) + 3;
+                    for (size_t j = i; j < i + count; j += 1)
+                        raw_code_lens[j] = raw_code_lens[i - 1];
+                    i += count - 1;
+                }
+                else if (inst == 17)
+                {
+                    uint8_t count = bits_pop(&input, 3) + 3;
+                    for (size_t j = i; j < i + count; j += 1)
+                        raw_code_lens[j] = 0;
+                    i += count - 1;
+                }
+                else if (inst == 18)
+                {
+                    uint8_t count = bits_pop(&input, 7) + 11;
+                    for (size_t j = i; j < i + count; j += 1)
+                        raw_code_lens[j] = 0;
+                    i += count - 1;
+                }
+                else
+                    ASSERT_OR_BROKEN_FILE(0, ret)
+                //printf("set len of code %d to %d\n", i, raw_code_lens[i]);
+            }
+            
+            uint8_t code_lens[288] = {0};
+            uint16_t code_lits[1 << 15] = {0};
+            uint16_t code_by_len[16] = {0};
+            memcpy(code_lens, raw_code_lens, len_count);
+            build_code(code_lens, code_lits, code_by_len, 288);
+            
+            uint8_t dist_code_lens[32] = {0};
+            uint16_t dist_code_lits[1 << 15] = {0};
+            uint16_t dist_code_by_len[16] = {0};
+            memcpy(dist_code_lens, &raw_code_lens[len_count], dist_count);
+            build_code(dist_code_lens, dist_code_lits, dist_code_by_len, 32);
+            
+            //for (size_t i = 0; i <= 15; i += 1)
+            //    printf("by len %d: %d\n", i, code_by_len[i]);
+            //
+            //for (size_t i = 0; i <= 15; i += 1)
+            //    printf("by len %d: %d\n", i, dist_code_by_len[i]);
+            
+            int lz77_error = 0;
+            do_lz77(&input, &ret, code_lits, code_by_len, dist_code_lits, dist_code_by_len, &lz77_error);
+            ASSERT_OR_BROKEN_FILE(lz77_error == 0, ret)
+        }
+        else
+            ASSERT_OR_BROKEN_FILE(0, ret)
+        
+        // if we tried to read past the end of the input
+        if (input.byte_index > input.buffer.len)
+            ASSERT_OR_BROKEN_FILE(0, ret)
+        else if (input.byte_index == input.buffer.len && input.bit_index != 0)
+            ASSERT_OR_BROKEN_FILE(0, ret)
+        
+        if (final)
+            break;
+    }
+    
+    return ret;
+}
+
+#undef ASSERT_OR_BROKEN_FILE
+#undef ASSERT_OR_BROKEN_DECODER
---
+++ main.c
@@ -0,0 +1,190 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#define SINFL_IMPLEMENTATION
+#include "sinfl.h"
+
+#include "inflate.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include "buffers.h"
+
+void cb_bytes_push(void * userdata, const unsigned char * bytes, size_t count)
+{
+    bytes_push((byte_buffer *)userdata, bytes, count);
+}
+void cb_bytes_copy(void * userdata, size_t distance, size_t count)
+{
+    byte_buffer * buf = (byte_buffer *)userdata;
+    if (distance > buf->len)
+        return;
+    for (size_t i = 0; i < count; i += 1)
+        byte_push(buf, buf->data[buf->len - distance]);
+}
+void cb_verify_checksum(void * userdata, unsigned (*checksummer)(const unsigned char *, size_t), size_t count, unsigned expected_checksum)
+{
+    byte_buffer * buf = (byte_buffer *)userdata;
+    uint32_t actual_checksum = checksummer(buf->data, count < buf->len ? count : buf->len);
+    if (expected_checksum != actual_checksum)
+    {
+        printf("checksums %08X and %08X don't match!!!\n", expected_checksum, actual_checksum);
+        printf("%lld %lld %lld\n", buf->len, count, (size_t)userdata);
+    }
+}
+
+void wpng_load_and_save(byte_buffer * buf)
+{
+	if (buf->len < 8)
+		return;
+	if (memcmp(buf->data, "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", 8) != 0)
+		return;
+    buf->cur = 8;
+    
+    byte_buffer idat;
+    memset(&idat, 0, sizeof(byte_buffer));
+    
+    uint32_t width = 0;
+    uint32_t height = 0;
+    uint8_t bit_depth = 0;
+    uint8_t color_type = 0;
+    uint8_t interlacing = 0;
+    
+    while (buf->cur < buf->len)
+    {
+        uint32_t size = byteswap_int(bytes_pop_int(buf, 4), 4);
+        
+        char name[5] = {0};
+        for (size_t i = 0; i < 4; i += 1)
+            name[i] = byte_pop(buf);
+        
+        printf("found chunk: %s\n", name);
+        
+        size_t cur_start = buf->cur;
+        
+        if (memcmp(name, "IHDR", 4) == 0)
+        {
+            width = byteswap_int(bytes_pop_int(buf, 4), 4);
+            height = byteswap_int(bytes_pop_int(buf, 4), 4);
+            bit_depth = byte_pop(buf);
+            color_type = byte_pop(buf);
+            assert(byte_pop(buf) == 0); // 0
+            assert(byte_pop(buf) == 0); // 0
+            interlacing = byte_pop(buf);
+        }
+        else if (memcmp(name, "IDAT", 4) == 0)
+            bytes_push(&idat, &buf->data[buf->cur], size);
+        else if (memcmp(name, "IEND", 4) == 0)
+            break;
+        
+        buf->cur = cur_start + size + 4;
+    }
+    
+    bytes_reserve(&idat, idat.cap + 8);
+    
+    assert(bit_depth == 8);
+    assert(color_type == 0 || color_type == 2 || color_type == 4 || color_type == 6);
+    assert(interlacing == 0);
+    
+    uint8_t temp = color_type / 2;
+    uint8_t bpp = (((temp & 1) << 1) + ((temp & 2) >> 1)) + 1;
+    /*
+    byte_buffer dec;
+    memset(&dec, 0, sizeof(byte_buffer));
+    struct sinfl_callbacks callbacks;
+    callbacks.userdata = (void *)&dec;
+    callbacks.bytes_push = cb_bytes_push;
+    callbacks.bytes_copy = cb_bytes_copy;
+    callbacks.verify_checksum = cb_verify_checksum;
+    
+    zsinflate(idat.data, idat.len, &callbacks); // decompresses into `dec` (declared earlier)
+    */
+    byte_buffer buf2 = {&idat.data[2], idat.len - 6, idat.len - 6, 0};
+    int error = 0;
+    byte_buffer dec = do_inflate(&buf2, &error); // decompresses into `dec` (declared earlier)
+    dec.cur = 0;
+    printf("%d\n", error);
+    
+    uint8_t * image_data = malloc(height * width * bpp);
+    memset(image_data, 0, height * width * bpp);
+    
+    for (size_t y = 0; y < height; y++)
+    {
+        uint8_t filter_type = byte_pop(&dec);
+        assert(dec.cur + width * bpp <= dec.len);
+        memcpy(&image_data[y * width * bpp], &dec.data[dec.cur], width * bpp);
+        dec.cur += width * bpp;
+        
+        if (filter_type == 1)
+        {
+            for (size_t x = bpp; x < width * bpp; x++)
+                image_data[y * width * bpp + x] += image_data[y * width * bpp + x - bpp];
+        }
+        if (filter_type == 2 && y > 0)
+        {
+            for (size_t x = 0; x < width * bpp; x++)
+                image_data[y * width * bpp + x] += image_data[(y - 1) * width * bpp + x];
+        }
+        if (filter_type == 3)
+        {
+            for (size_t x = 0; x < width * bpp; x++)
+            {
+                uint16_t up   = y >    0 ? image_data[(y - 1) * width * bpp + x] : 0;
+                uint16_t left = x >= bpp ? image_data[y * width * bpp + x - bpp] : 0;
+                uint8_t avg = (up + left) / 2;
+                image_data[y * width * bpp + x] += avg;
+            }
+        }
+        if (filter_type == 4)
+        {
+            for (size_t x = 0; x < width * bpp; x++)
+            {
+                int16_t left   = x >= bpp ? image_data[y * width * bpp + x - bpp] : 0;
+                int16_t up     = y >    0 ? image_data[(y - 1) * width * bpp + x] : 0;
+                int16_t upleft = y >    0 &&
+                                 x >= bpp ? image_data[(y - 1) * width * bpp + x - bpp] : 0;
+                
+                int16_t lin = left + up - upleft;
+                int16_t diff_left   = abs(lin - left);
+                int16_t diff_up     = abs(lin - up);
+                int16_t diff_upleft = abs(lin - upleft);
+                
+                int16_t ref = 0;
+                if (diff_left <= diff_up && diff_left <= diff_upleft)
+                    ref = left;
+                else if (diff_up <= diff_upleft)
+                    ref = up;
+                else
+                    ref = upleft;
+                
+                image_data[y * width * bpp + x] += ref;
+            }
+        }
+    }
+    
+    stbi_write_png("out.png", width, height, bpp, image_data, width * bpp);
+}
+
+int main()
+{
+    //FILE * f = fopen("unifont-jp.png", "rb");
+    FILE * f = fopen("0-ufeff_tiles_v2.png", "rb");
+    
+    fseek(f, 0, SEEK_END);
+    size_t file_len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    
+    uint8_t * raw_data = (uint8_t *)malloc(file_len);
+    fread(raw_data, file_len, 1, f);
+    byte_buffer in_buf = {raw_data, file_len, file_len, 0};
+    
+    fclose(f);
+    
+    wpng_load_and_save(&in_buf);
+    
+	return 0;
+}
