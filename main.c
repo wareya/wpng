693c3051d2684dfe2367300489333531bdc48a42
wareya <wareya@gmail.com>
2024-01-03 18:39:53 -0500

    implement saving palettized images

--- main.c
+++ main.c
@@ -42,6 +42,23 @@ uint8_t paeth_get_ref(uint8_t * image_data, uint32_t bytes_per_scanline, uint32_
     return paeth_get_ref_raw(left, up, upleft);
 }
 
+float to_srgb(float x)
+{
+    if (x > 0.0031308)
+        return 1.055 * (pow(x, (1.0 / 2.4))) - 0.055;
+    else
+        return 12.92 * x;
+}
+
+uint16_t apply_gamma_u16(uint16_t val, float gamma)
+{
+    return round(to_srgb(pow(val / 65535.0, gamma)) * 65535.0);
+}
+uint8_t apply_gamma_u8(uint8_t val, float gamma)
+{
+    return round(to_srgb(pow(val / 255.0, gamma)) * 255.0);
+}
+
 void apply_gamma(uint32_t width, uint32_t height, uint8_t bpp, uint8_t is_16bit, uint8_t * image_data, size_t bytes_per_scanline, float gamma)
 {
     uint8_t components = bpp;
@@ -59,26 +76,172 @@ void apply_gamma(uint32_t width, uint32_t height, uint8_t bpp, uint8_t is_16bit,
                 uint16_t a = image_data[y * bytes_per_scanline + x * 2 + 0];
                 uint16_t b = image_data[y * bytes_per_scanline + x * 2 + 1];
                 uint16_t val = (a << 8) | b;
-                val = pow(val / 65535.0, gamma) * 65535.0;
+                val = apply_gamma_u16(val, gamma);
                 image_data[y * bytes_per_scanline + x * 2 + 0] = val >> 8;
                 image_data[y * bytes_per_scanline + x * 2 + 1] = val;
             }
             else
             {
                 
-                uint8_t a = image_data[y * bytes_per_scanline + x];
-                a = pow(a / 255.0, gamma) * 255.0;
-                image_data[y * bytes_per_scanline + x] = a;
+                uint8_t val = image_data[y * bytes_per_scanline + x];
+                val = apply_gamma_u16(val, gamma);
+                image_data[y * bytes_per_scanline + x] = val;
             }
         }
     }
 }
-void wpng_write(const char * filename, uint32_t width, uint32_t height, uint8_t bpp, uint8_t is_16bit, uint8_t * image_data, size_t bytes_per_scanline)
+
+inline static int luma_compare(const void * _a, const void * _b)
+{
+    uint32_t a = (*((uint32_t*)_a)) >> 8;
+    uint32_t b = (*((uint32_t*)_b)) >> 8;
+    uint32_t luma_a = (a & 0xFF) * 2 + (a >> 16) * 3 + ((a >> 8) & 0xFF) * 5;
+    uint32_t luma_b = (b & 0xFF) * 2 + (b >> 16) * 3 + ((b >> 8) & 0xFF) * 5;
+    return luma_a > luma_b ? 1 : luma_a < luma_b ? -1 : 0;
+}
+
+uint32_t pal_val_expand(uint32_t val, uint8_t bpp)
+{
+    if (bpp == 1)
+    {
+        val |= val << 8;
+        val |= val << 16;
+        val |= 0xFF;
+    }
+    if (bpp == 2)
+    {
+        val |= val >> 8 << 16;
+        val |= val >> 8 << 24;
+    }
+    if (bpp == 3)
+    {
+        val <<= 8;
+        val |= 0xFF;
+    }
+    return val;
+}
+
+// image must be 8-bit y, ya, rgb, or rgba. does not support 16-bit.
+// returns null on failure, pointer on success
+uint8_t * palettize(uint32_t width, uint32_t height, uint8_t bpp, uint8_t * image_data, size_t bytes_per_scanline, size_t * size, uint32_t * pal, uint32_t * pal_count, uint32_t * arg_depth)
+{
+    uint32_t palette[256] = {0};
+    uint32_t palette_i = 0;
+    
+    for (size_t y = 0; y < height; y += 1)
+    {
+        for (size_t x = 0; x < width; x += 1)
+        {
+            uint32_t val = 0;
+            for (size_t j = 0; j < bpp; j += 1)
+            {
+                val <<= 8;
+                val |= image_data[y * bytes_per_scanline + x * bpp + j];
+            }
+            
+            val = pal_val_expand(val, bpp);
+            size_t index = 0;
+            while (index < 256 && palette[index] != val)
+                index += 1;
+            
+            if (index == 256)
+            {
+                if (palette_i == 256)
+                    return 0;
+                else
+                {
+                    palette[palette_i] = val;
+                    palette_i += 1;
+                }
+            }
+        }
+    }
+    // sort palette by luma to help png's byte filter compress better
+    if (palette_i > 0)
+        qsort(palette, palette_i, sizeof(uint32_t), luma_compare);
+    else
+        return 0;
+    
+    printf("bpp: %d\n", bpp);
+    for (size_t i = 0; i < palette_i; i += 1)
+        printf("%08X\n", palette[i]);
+    
+    uint8_t depth = 0;
+    if (palette_i <= 2)
+        depth = 1;
+    else if (palette_i <= 4)
+        depth = 2;
+    else if (palette_i <= 16)
+        depth = 4;
+    else if (palette_i <= 256)
+        depth = 8;
+    else
+        return 0;
+    
+    assert(palette_i <= 256);
+    
+    printf("depth: %d\n", depth);
+    
+    size_t out_bps = (width * depth + 7) / 8;
+    
+    uint8_t * output = (uint8_t *)malloc(out_bps * height);
+    memset(output, 0, out_bps * height);
+    
+    for (size_t y = 0; y < height; y += 1)
+    {
+        for (size_t x = 0; x < width; x += 1)
+        {
+            uint32_t val = 0;
+            for (size_t j = 0; j < bpp; j += 1)
+            {
+                val <<= 8;
+                val |= image_data[y * bytes_per_scanline + x * bpp + j];
+            }
+            val = pal_val_expand(val, bpp);
+            
+            uint32_t index = 0;
+            while (index < palette_i && palette[index] != val)
+                index += 1;
+            assert(index < palette_i);
+            
+            uint8_t n = x % (8 / depth);
+            size_t i = x / (8 / depth);
+            uint32_t orval = index << (uint32_t)(8 - ((n + 1) * depth));
+            assert(orval < 256);
+            output[y * out_bps + i] |= orval;
+        }
+    }
+    
+    if (size)
+        *size = out_bps * height;
+    if (arg_depth)
+        *arg_depth = depth;
+    if (pal_count)
+        *pal_count = palette_i;
+    if (pal)
+    {
+        for (size_t i = 0; i < palette_i; i += 1)
+            pal[i] = palette[i];
+    }
+    
+    return output;
+}
+void wpng_write(const char * filename, uint32_t width, uint32_t height, uint8_t bpp, uint8_t is_16bit, uint8_t * image_data, size_t bytes_per_scanline, uint8_t allow_palletization)
 {
     byte_buffer out;
     memset(&out, 0, sizeof(byte_buffer));
     
     bytes_push(&out, (const uint8_t *)"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", 8);
+    
+    //uint8_t * orig_image_data = image_data;
+    //uint8_t orig_bpp = bpp;
+    //size_t orig_bytes_per_scanline = bytes_per_scanline;
+    
+    size_t palettized_size = 0;
+    uint32_t palette[256] = {0};
+    uint32_t pal_count = 0;
+    uint32_t pal_depth = 0;
+    uint8_t * palettized = !allow_palletization || is_16bit ? 0 : palettize(width, height, bpp, image_data, bytes_per_scanline, &palettized_size, palette, &pal_count, &pal_depth);
     
     // write header chunk
     
@@ -87,14 +250,57 @@ void wpng_write(const char * filename, uint32_t width, uint32_t height, uint8_t 
     bytes_push(&out, (const uint8_t *)"IHDR", 4);
     bytes_push_int(&out, byteswap_int(width, 4), 4);
     bytes_push_int(&out, byteswap_int(height, 4), 4);
-    byte_push(&out, is_16bit ? 16 : 8);
-    uint8_t components = is_16bit ? bpp / 2 : bpp;
-    byte_push(&out, components == 1 ? 0 : components == 2 ? 4 : components == 3 ? 2 : components == 4 ? 6 : 0);
+    
+    if (!palettized)
+    {
+        byte_push(&out, is_16bit ? 16 : 8);
+        uint8_t components = is_16bit ? bpp / 2 : bpp;
+        byte_push(&out, components == 1 ? 0 : components == 2 ? 4 : components == 3 ? 2 : components == 4 ? 6 : 0);
+    }
+    else
+    {
+        byte_push(&out, pal_depth);
+        byte_push(&out, 3);
+        image_data = palettized;
+        bpp = 1;
+        bytes_per_scanline = palettized_size / height;
+    }
+    
     byte_push(&out, 0); // compression method (deflate)
     byte_push(&out, 0); // filter method (adaptive x5)
     byte_push(&out, 0); // interlacing method (none)
+    
     size_t chunk_size = out.len - chunk_start;
     bytes_push_int(&out, byteswap_int(defl_compute_crc32(&out.data[chunk_start], chunk_size, 0), 4), 4);
+    
+    if (palettized)
+    {
+        // palette chunk
+        bytes_push_int(&out, byteswap_int(pal_count * 3, 4), 4);
+        
+        chunk_start = out.len;
+        bytes_push(&out, (const uint8_t *)"PLTE", 4);
+        for (size_t i = 0; i < pal_count; i++)
+        {
+            byte_push(&out, palette[i] >> 24);
+            byte_push(&out, palette[i] >> 16);
+            byte_push(&out, palette[i] >> 8);
+        }
+        
+        size_t chunk_size = out.len - chunk_start;
+        bytes_push_int(&out, byteswap_int(defl_compute_crc32(&out.data[chunk_start], chunk_size, 0), 4), 4);
+        
+        // transparency chunk
+        bytes_push_int(&out, byteswap_int(pal_count, 4), 4);
+        
+        chunk_start = out.len;
+        bytes_push(&out, (const uint8_t *)"tRNS", 4);
+        for (size_t i = 0; i < pal_count; i++)
+            byte_push(&out, palette[i]);
+        
+        chunk_size = out.len - chunk_start;
+        bytes_push_int(&out, byteswap_int(defl_compute_crc32(&out.data[chunk_start], chunk_size, 0), 4), 4);
+    }
     
     // write IDAT chunks
     // first, collect pixel data
@@ -108,7 +314,7 @@ void wpng_write(const char * filename, uint32_t width, uint32_t height, uint8_t 
         
         uint64_t sum_abs_null = 0;
         uint64_t sum_abs_left = 0;
-        uint64_t sum_abs_top = -1;
+        uint64_t sum_abs_top = 0;
         uint64_t sum_abs_avg = 0;
         uint64_t sum_abs_paeth = 0;
         
@@ -128,38 +334,29 @@ void wpng_write(const char * filename, uint32_t width, uint32_t height, uint8_t 
         }
         
         for (size_t x = 0; x < bytes_per_scanline; x++)
+        {
             sum_abs_null += abs((int32_t)(int8_t)(image_data[start + x] - most_common_val));
+            
+            uint16_t up   = y >    0 ? image_data[start - bytes_per_scanline + x] : 0;
+            uint16_t left = x >= bpp ? image_data[start + x - bpp] : 0;
+            uint8_t avg = (up + left) / 2;
+            sum_abs_avg += abs((int32_t)(image_data[start + x]) - (int32_t)(avg));
+            
+            sum_abs_left += abs((int32_t)(image_data[start + x]) - (int32_t)left);
+            
+            sum_abs_top += abs((int32_t)(image_data[start + x]) - (int32_t)up);
+            
+            uint8_t ref = paeth_get_ref(image_data, bytes_per_scanline, x, y, bpp);
+            sum_abs_paeth += abs((int32_t)(image_data[start + x]) - (int32_t)(ref));
+        }
+        
+        if (y == 0)
+            sum_abs_top = -1;
         
         // bias in favor of storing unfiltered if enough (25%) earlier scanlines are stored unfiltered
         // this helps with deflate's lz77 pass
         if (num_unfiltered > y / height / 4)
             sum_abs_null /= 3;
-        
-        for (size_t x = 0; x < bpp; x++)
-            sum_abs_left += abs((int32_t)(int8_t)(image_data[start + x]));
-        for (size_t x = bpp; x < bytes_per_scanline; x++)
-            sum_abs_left += abs((int32_t)(image_data[start + x]) - (int32_t)(image_data[start + x - bpp]));
-        
-        if (y > 0)
-        {
-            sum_abs_top = 0;
-            for (size_t x = 0; x < bytes_per_scanline; x++)
-                sum_abs_top += abs((int32_t)(image_data[start + x]) - (int32_t)(image_data[start - bytes_per_scanline + x]));
-        }
-        
-        for (size_t x = 0; x < bytes_per_scanline; x++)
-        {
-            uint16_t up   = y >    0 ? image_data[start - bytes_per_scanline + x] : 0;
-            uint16_t left = x >= bpp ? image_data[start + x - bpp] : 0;
-            uint8_t avg = (up + left) / 2;
-            sum_abs_avg += abs((int32_t)(image_data[start + x]) - (int32_t)(avg));
-        }
-        
-        for (size_t x = 0; x < bytes_per_scanline; x++)
-        {
-            uint8_t ref = paeth_get_ref(image_data, bytes_per_scanline, x, y, bpp);
-            sum_abs_paeth += abs((int32_t)(image_data[y * bytes_per_scanline + x]) - (int32_t)(ref));
-        }
         
         if (sum_abs_null <= sum_abs_left && sum_abs_null <= sum_abs_top && sum_abs_null <= sum_abs_avg && sum_abs_null <= sum_abs_paeth)
         {
@@ -207,8 +404,9 @@ void wpng_write(const char * filename, uint32_t width, uint32_t height, uint8_t 
             }
         }
     }
-    printf("raw pixel data byte count: %lld\n", pixel_data.len);
-    bit_buffer pixel_data_comp = do_deflate(pixel_data.data, pixel_data.len, 12, 1);
+    
+    printf("raw pixel data byte count: %zu\n", pixel_data.len);
+    bit_buffer pixel_data_comp = do_deflate(pixel_data.data, pixel_data.len, 9, 1);
     
     bytes_push_int(&out, byteswap_int(pixel_data_comp.buffer.len, 4), 4);
     chunk_start = out.len;
@@ -225,8 +423,9 @@ void wpng_write(const char * filename, uint32_t width, uint32_t height, uint8_t 
     fclose(f);
     
     printf("bpp %d\n", bpp);
-    
     puts("saved");
+    
+    free(palettized);
 }
 
 void defilter(uint8_t * image_data, byte_buffer * dec, uint32_t width, uint32_t height, uint8_t interlace_layer, uint8_t bit_depth, uint8_t components)
@@ -413,7 +612,7 @@ void wpng_load_and_save(byte_buffer * buf)
         {
             assert(palette_size == 0); // can't come after palette
             assert(!has_idat); // can't come after idat
-            gamma = 1.0 / (byteswap_int(bytes_pop_int(buf, 4), 4) / 100000.0 * 2.2);
+            gamma = 100000.0 / byteswap_int(bytes_pop_int(buf, 4), 4);
         }
         else if (memcmp(name, "cHRM", 4) == 0)
         {
@@ -501,11 +700,6 @@ void wpng_load_and_save(byte_buffer * buf)
     assert(has_idat);
     assert(has_iend);
     assert(width != 0 && height != 0); // header must exist and give valid width/height values
-    
-    palette[0] = palette[0];
-    transparent_r = transparent_r;
-    transparent_g = transparent_g;
-    transparent_b = transparent_b;
     
     if (color_type == 4 || color_type == 6)
         assert(has_trns == 0);
@@ -681,13 +875,13 @@ void wpng_load_and_save(byte_buffer * buf)
         }
         if (gamma >= 0.0 && !is_srgb)
             apply_gamma(width, height, out_bpp, bit_depth == 16, out_image_data, width * out_bpp, gamma);
-        wpng_write("out.png", width, height, out_bpp, bit_depth == 16, out_image_data, width * out_bpp);
+        wpng_write("out.png", width, height, out_bpp, bit_depth == 16, out_image_data, width * out_bpp, 1);
     }
     else
     {
         if (gamma >= 0.0 && !is_srgb)
             apply_gamma(width, height, bpp, bit_depth == 16, image_data, bytes_per_scanline, gamma);
-        wpng_write("out.png", width, height, bpp, bit_depth == 16, image_data, bytes_per_scanline);
+        wpng_write("out.png", width, height, bpp, bit_depth == 16, image_data, bytes_per_scanline, 1);
     }
 }
 
